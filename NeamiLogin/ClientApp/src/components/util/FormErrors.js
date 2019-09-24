9985338d6f364f6bb8be17c7eb44338b9d0f9a70
drzeeman <zawarq@gmail.com>
2019-09-24 17:34:50 +1000

    Created new project

---
+++ NeamiLogin/ClientApp/src/components/util/FormErrors.js
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+
+export const FormErrors = ({ formErrors }) =>
+    <div className='form-errors'>
+        <ul>
+        {Object.keys(formErrors).map((fieldName, i) => {
+            if (formErrors[fieldName].length > 0) {
+                return (
+                    <li className="text-danger" key={i}>{formErrors[fieldName]}</li>
+                )
+            } else {
+                return '';
+            }
+            })}
+        </ul>
+    </div>
